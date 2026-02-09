4cc7eb9354244f61abd2a851b42f317668cc2b8f
cristinalin <cristinalin2012@gmail.com>
2026-02-09 10:32:30 +0000

    update returns

--- risklib/utils/returns.py
+++ risklib/utils/returns.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 def portfolio_returns(dataframe, weights, log_returns = False):
-    if isinstance(weights, pd.DataFrame):
-        weights = weights.iloc[0]
-    if isinstance(weights, pd.Series):
+    if type(weights) == pd.DataFrame:
+        weights = weights.iloc[1, 1:]
+    if type(weights) == pd.Series:
         weights = weights.values
     else:
         weights = np.array(weights)

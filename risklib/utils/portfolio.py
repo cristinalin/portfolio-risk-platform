14004b13250c7118598f816bbc23972e8794f8da
cristinalin <cristinalin2012@gmail.com>
2026-02-09 10:41:21 +0000

    portfolio returns

--- risklib/utils/portfolio.py
+++ risklib/utils/portfolio.py
@@ -1,5 +1,13 @@
 import numpy as np
 import pandas as pd
+from risklib.utils.covariance import calculate_cov_matrix
+
+def portfolio_std_dev(returns, weights):
+    cov_matrix = calculate_cov_matrix(returns, weights)
+    w = weights.values.reshape(-1, 1)
+    sigma2_p = (w.T @ cov_matrix.values @ w).item()
+    
+    return np.sqrt(sigma2_p)
 
 def portfolio_returns(dataframe, weights, log_returns = False):
     if type(weights) == pd.DataFrame:
--- risklib/utils/volatility.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from risklib.utils.covariance import calculate_cov_matrix
-
-def portfolio_std_dev(returns, weights):
-    cov_matrix = calculate_cov_matrix(returns, weights)
-    w = weights.values.reshape(-1, 1)
-    sigma2_p = (w.T @ cov_matrix.values @ w).item()
-    
-    return np.sqrt(sigma2_p)
